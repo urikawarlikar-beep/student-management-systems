1e2ea3ab60e62b3a9e277b327a000f26e6d4ba16
urikawarlikar-beep <urika.warlikar@sdbi.in>
2026-07-17 10:21:13 +0530

    Initial commit

---
+++ student.py
@@ -0,0 +1,22 @@
+students = {}
+
+def add_student(student_id, name):
+    if student_id in students:
+        return False
+    students[student_id] = name
+    return True
+
+def remove_student(student_id):
+    if student_id in students:
+        del students[student_id]
+        return True
+    return False
+
+def search_student(student_id):
+    return students.get(student_id)
+
+def update_student(student_id, name):
+    if student_id in students:
+        students[student_id] = name
+        return True
+    return False
